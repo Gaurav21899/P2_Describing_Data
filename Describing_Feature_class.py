edb49f83d32cbf287b0386a09ace0ed408ba9d85
Gaurav21899 <gaurav21899@gmail.com>
2023-09-06 16:50:03 +0530

    Modified Describing Data

--- Describing_Feature_class.py
+++ Describing_Feature_class.py
@@ -1,18 +1,19 @@
 import arcpy
 import os
 
-file_GDB_path = r"D:\Sem-3\Programming for GIS-3\P2_Describing_Data\Practical_2_ProProject\Practical_2_ProProject\02_Describing_Data.gdb"
+# Path to the GDB
+file_GDB_path = r"D:\Sem-3\Programming for GIS-3\P2_Describing_Data\Practical_2_ProProject\02_Describing_Data.gdb"
 
 # Feature class names
 majorAttractions_fc_name = "MajorAttractions"
-freeways_fc_name = "Freeways"
+freeways_fc_name = "freeways"
 
 # Joining the path
-majorAttraction_fc_full_path = os.path.join(file_GDB_path,majorAttractions_fc_name)
+majorAttraction_fc_full_path = os.path.join(file_GDB_path, majorAttractions_fc_name)
 print(majorAttraction_fc_full_path)
-freeways_fc_full_path = os.path.join(file_GDB_path,freeways_fc_name)
+freeways_fc_full_path = os.path.join(file_GDB_path, freeways_fc_name)
 
-majorAttractions_Feature_class_path = r"D:\Sem-3\Programming for GIS-3\P2_Describing_Data\Practical_2_ProProject\Practical_2_ProProject\02_Describing_Data.gdb\MajorAttractions"
+majorAttractions_Feature_class_path = r"D:\Sem-3\Programming for GIS-3\P2_Describing_Data\Practical_2_ProProject\02_Describing_Data.gdb\MajorAttractions"
 freeways_desc_obj = arcpy.Describe(freeways_fc_full_path)
 
 # Creating a Describe object
